ca411053c358f604395c2b82b568c1fb2c72ca1d
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-13 14:26:32 -0500

    base mode complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,17 +2,17 @@ var app = angular.module('HeroApp', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
-    .when('/heroList', {
-      templateUrl: '/views/hero-list.html',
-      controller: 'HeroListController',
-      controllerAs: 'hlc'
-    })
-    .when('/addHero', {
-      templateUrl: '/views/add-hero.html',
-      controller: 'AddHeroController',
-      controllerAs: 'ah'
-    })
-    .otherwise({
-      redirectTo: 'heroList'
-    })
+  .when('/heroList', {
+    templateUrl: '/views/hero-list.html',
+    controller: 'HeroListController',
+    controllerAs: 'hlc'
+  })
+  .when('/addHero', {
+    templateUrl: '/views/add-hero.html',
+    controller: 'AddHeroController',
+    controllerAs: 'ah'
+  })
+  .otherwise({
+    redirectTo: 'heroList'
+  })
 }]);
--- server/public/scripts/controllers/add-hero.js
+++ server/public/scripts/controllers/add-hero.js
@@ -1,7 +1,19 @@
 app.controller('AddHeroController', ['$http', function($http){
-    console.log('Add hero loaded');
+  console.log('Add controller hero loaded');
 
-    // var self = this;
+  var self = this;
+  self.newHero = {};
+  var newHeroData = self.newHero;
 
-
+  self.addHero = function(){
+    console.log('add hero button clicked');
+    $http({
+      method: 'POST',
+      url: '/heroes',
+      data: newHeroData
+    }).then(function(response){
+      console.log(response);
+    });
+    self.newHero = null;
+  }
 }]); // end of myApp controller
