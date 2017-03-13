a54a4817a40b07c99c805cce6e2315185ea48c62
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-13 14:09:51 -0500

    Add hero controller loaded and form built

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,6 +7,11 @@ app.config(['$routeProvider', function($routeProvider) {
       controller: 'HeroListController',
       controllerAs: 'hlc'
     })
+    .when('/addHero', {
+      templateUrl: '/views/add-hero.html',
+      controller: 'AddHeroController',
+      controllerAs: 'ah'
+    })
     .otherwise({
       redirectTo: 'heroList'
     })
---
+++ server/public/scripts/controllers/add-hero.js
@@ -0,0 +1,7 @@
+app.controller('AddHeroController', ['$http', function($http){
+    console.log('Add hero loaded');
+
+    // var self = this;
+
+
+}]); // end of myApp controller
