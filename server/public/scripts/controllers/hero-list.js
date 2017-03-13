a803a512046f90d6cab1a26141cd3520e8c34c10
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-13 13:58:05 -0500

    GET and DELETE routes working

--- server/public/scripts/controllers/hero-list.js
+++ server/public/scripts/controllers/hero-list.js
@@ -1,3 +1,29 @@
-app.controller('HeroListController', function(){
+app.controller('HeroListController', ['$http', function($http){
     console.log('Hero List Controller loaded');
-});
+
+    var self = this;
+    var heroList = {list: []};
+
+    getHeroes();
+
+    function getHeroes(){
+      $http({
+        method: 'GET',
+        url: '/heroes'
+      }).then(function(response) {
+        console.log('response from server: ', response);
+        heroList.list = response.data;
+        self.heroArray = heroList.list;
+      });
+} // end of getHeroes function
+
+self.deleteHero = function(heroId){
+  $http({
+    method: 'DELETE',
+    url: '/heroes/' + heroId
+  }).then(function(response) {
+    getHeroes();
+  });
+}
+
+}]); // end of myApp controller
